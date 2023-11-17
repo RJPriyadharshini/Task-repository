da5e7eaecf5a87081f5ed81ffd6dfb58a67ed01d
RJPriyadharshini <142367764+RJPriyadharshini@users.noreply.github.com>
2023-11-17 21:59:54 +0530

    Update Task-10.py

--- Task-10.py
+++ Task-10.py
@@ -42,8 +42,8 @@ class Audio:
         if 1 <= rating <= 5:
             self.ratings.append(rating)
 
-    def calculate_average_rating(self):
-        return sum(self.ratings) / len(self.ratings) if self.ratings else 0
+    def audio_average_rating(self):
+        return sum(self.ratings) / len(self.ratings)
 
 class Playlist:
     def __init__(self, name, genre):
@@ -59,9 +59,8 @@ class Playlist:
         if 1 <= rating <= 5:
             self.ratings.append(rating)
 
-    def calculate_average_rating(self):
-        return sum(self.ratings) / len(self.ratings) if self.ratings else 0
-
+    def playlist_average_rating(self):
+        return sum(self.ratings) / len(self.ratings) 
 class User:
     def __init__(self, name):
         self.name = name
@@ -98,9 +97,9 @@ class MusicPlayer:
 player = MusicPlayer()
 
 # Creating users
-user1 = player.create_user("Alice")
-user2 = player.create_user("Bob")
-user3 = player.create_user("Charlie")
+user1 = player.create_user("Anitha")
+user2 = player.create_user("Priya")
+user3 = player.create_user("Pavi")
 
 # Creating playlists
 playlist1 = player.create_playlist("Pop Hits", "Pop")
@@ -115,7 +114,7 @@ playlist1.add_audio(audio1)
 playlist1.add_audio(audio2)
 playlist2.add_audio(audio3)
 
-# Users rating audios and playlists
+# Users give the random rating in audios and playlists
 user1.rate_audio(audio1, random.randint(1, 5))
 user2.rate_audio(audio1, random.randint(1, 5))
 user3.rate_audio(audio2, random.randint(1, 5))
@@ -125,109 +124,13 @@ user2.rate_playlist(playlist1, random.randint(1, 5))
 user3.rate_playlist(playlist2, random.randint(1, 5))
 
 # Calculating average ratings
-average_rating_playlist1 = playlist1.calculate_average_rating()
-average_rating_audio1 = audio1.calculate_average_rating()
+average_rating_audio1 = audio1.audio_average_rating()
+average_rating_playlist1 = playlist1.playlist_average_rating()
+
+#print the average rating
 
 print(f"Average rating for Playlist 1: {average_rating_playlist1}")
 print(f"Average rating for Audio 1: {average_rating_audio1}")
-
-
-'''
-import random
-
-class Audio:
-    def __init__(self, title, url):
-        self.title = title
-        self.url = url
-        self.ratings = []
-
-    def add_rating(self, rating):
-        self.ratings.append(rating)
-
-    def calculate_average_rating(self):
-        if not self.ratings:
-            return 0
-        return sum(self.ratings) / len(self.ratings)
-
-class Playlist:
-    def __init__(self, name, genre):
-        self.name = name
-        self.genre = genre
-        self.audio_files = []
-
-    def add_audio(self, audio):
-        self.audio_files.append(audio)
-
-    def search_audio_by_name(self, name):
-        # Implement audio search logic by name
-        pass
-
-    def calculate_playlist_average_rating(self):
-        audio_ratings = [audio.calculate_average_rating() for audio in self.audio_files]
-        if not audio_ratings:
-            return 0
-        return sum(audio_ratings) / len(audio_ratings)
-
-class User:
-    def __init__(self, name):
-        self.name = name
-
-# Generating 3 users and random ratings for demonstration
-users = [User(f"User{i}") for i in range(1, 4)]
-audio = Audio("Sample Audio", "audio-url")
-for user in users:
-    random_rating = random.randint(1, 5)
-    audio.add_rating(random_rating)
-
-# Displaying the average rating for the audio
-print(f"Average Rating for the Audio: {audio.calculate_average_rating()}")
-
-import random
-class Audio:
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
-        self.rating = 0
-
-     def add_rating(self, rating):
-        self.ratings.append(rating)
-
-    def calculate_average_rating(self):
-        if not self.ratings:
-            return 0
-        return sum(self.ratings) / len(self.ratings)
-
-class Playlist:
-    def __init__(self, name, genre):
-        self.name = name
-        self.genre = genre
-        self.audios = []
-
-    def add_audio(self, audio):
-        self.audios.append(audio)
-
-    def calculate_average_rating(self):
-        total_ratings = sum(audio.rating for audio in self.audios)
-        return total_ratings / len(self.audios) if self.audios else 0
-
-
-# Example usage:
-playlist1 = Playlist("Playlist 1", "Pop")
-playlist2 = Playlist("Playlist 2", "Rock")
-
-audios = [Audio(f"Song {i}", f"https://example.com/song{i}.mp3") for i in range(1, 4)]
-for audio in audios:
-    audio.add_rating(random.randint(1, 5))
-
-playlist1.add_audio(audios[0])
-playlist1.add_audio(audios[1])
-playlist2.add_audio(audios[2])
-
-average_rating1 = playlist1.calculate_average_rating()
-average_rating2 = playlist2.calculate_average_rating()
-
-print(f"Average rating for Playlist 1: {average_rating1}")
-print(f"Average rating for Playlist 2: {average_rating2}") '''
 
 
 
